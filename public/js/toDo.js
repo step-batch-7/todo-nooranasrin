c2ebd14cdedd011872f467d1751f6b3dc7253e50
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2020-02-02 11:14:38 +0530

    Added a function to save the data and handle post request

--- public/js/toDo.js
+++ public/js/toDo.js
@@ -1,13 +1,30 @@
-const popupTodoMaker = function() {
-  document.getElementById('popupDiv').style.display = 'block';
-  document.getElementById('addButton').style.display = 'none';
+let lists = [];
+
+const sendXHR = function(data, url, method) {
+  const request = new XMLHttpRequest();
+  request.open(method, url);
+  request.send(data);
+  request.onload = function() {
+    console.log(this);
+  };
 };
 
 const hideToDoRegisterWindow = function() {
+  lists.push(document.getElementById('item').value);
+  const title = document.getElementById('title').value;
+  const todo = `title=${title}&lists=${JSON.stringify(lists)}`;
+  sendXHR(todo, '/saveTodo', 'POST');
   document.getElementById('addButton').style.display = 'block';
   document.getElementById('popupDiv').style.display = 'none';
 };
 
+const popupTodoMaker = function() {
+  lists = [];
+  document.getElementById('popupDiv').style.display = 'block';
+  document.getElementById('addButton').style.display = 'none';
+};
+
 const addItem = function() {
+  lists.push(document.getElementById('item').value);
   document.getElementById('item').value = '';
 };
