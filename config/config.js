61ed997824f4d9c0b782076f459affa700a3d370
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2020-02-08 18:12:50 +0530

    Added config file

---
+++ config/config.js
@@ -0,0 +1,6 @@
+const getStoragePath = function() {
+  return process.env.STORAGE_FILE || `${__dirname}/../data/todoLists.json`;
+};
+
+const storagePath = getStoragePath();
+module.exports = storagePath;
