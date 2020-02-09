4dada293ed276e28b54a497061c5729a768cedf8
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2020-02-09 18:30:26 +0530

    Deleted Data directory from the commit

--- config/config.js
+++ config/config.js
@@ -1,6 +1,15 @@
+const fs = require('fs');
+
+const createDirectoryIfNotExists = function() {
+  if (!fs.existsSync(`${__dirname}/../data`)) {
+    fs.mkdirSync(`${__dirname}/../data`);
+  }
+};
+
 const getStoragePath = function() {
   return process.env.STORAGE_FILE || `${__dirname}/../data/todoLists.json`;
 };
 
+createDirectoryIfNotExists();
 const storagePath = getStoragePath();
 module.exports = storagePath;
