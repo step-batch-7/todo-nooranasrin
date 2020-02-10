aa8e8feb1cff398e2859ed80f92ee951f06a9459
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2020-02-10 12:25:04 +0530

    Added registration form validation

--- public/js/loginAndRegistration.js
+++ public/js/loginAndRegistration.js
@@ -1,7 +1,38 @@
-const showSignUpPage = function() {
-  changeDisplayStyle('#registrationForm', '#loginForm');
+/* eslint-disable max-statements */
+const isEmpty = function(userDetails) {
+  return userDetails.some(input => input === '');
 };
 
-const showSignInPage = function() {
-  changeDisplayStyle('#loginForm', '#registrationForm');
+const isValidEmail = function(email) {
+  return email.includes('@') && email.includes('.');
+};
+
+const isValidNumber = function(phoneNumber) {
+  const isInteger = Number.isInteger(+phoneNumber);
+  return isInteger && phoneNumber.length === 10 && +phoneNumber > 0;
+};
+
+const showErrorMsg = function(msg, querySelector) {
+  document.querySelector(querySelector).innerText = msg;
+};
+
+const validateRegistrationForm = function() {
+  const userName = document.forms['registrationForm']['userName'].value;
+  const password = document.forms['registrationForm']['password'].value;
+  const email = document.forms['registrationForm']['email'].value;
+  const phoneNumber = document.forms['registrationForm']['phoneNumber'].value;
+
+  if (isEmpty([userName, password, email, phoneNumber])) {
+    showErrorMsg('* Please Fill All The Fields', '#errorInName');
+    return false;
+  }
+  if (!isValidEmail(email)) {
+    showErrorMsg('* Please Enter A Valid Email', '#errorInEmail');
+    return false;
+  }
+  if (!isValidNumber(phoneNumber)) {
+    showErrorMsg('* Please Enter A Valid Phone Number', '#errorInPhoneNumber');
+    return false;
+  }
+  return true;
 };
