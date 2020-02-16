76d5cb8a6fd1a78227623c6d5f97cfed79f417e9
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2020-02-16 11:33:13 +0530

    Added signIn

--- public/js/loginAndRegistration.js
+++ public/js/loginAndRegistration.js
@@ -13,3 +13,16 @@ const validateUserName = function() {
   const userName = event.target.value;
   postJSON('/checkUserNameExists', { userName }, checkUserNameIsAvailable);
 };
+
+const isValidLogin = function({ isValid, msg }) {
+  if (isValid) {
+    return (document.location = 'home.html');
+  }
+  document.querySelector('#errorInUserName').innerText = msg;
+};
+
+const validateLogin = function() {
+  const userName = document.querySelector('#username').value;
+  const password = document.querySelector('#password').value;
+  postJSON('/validateLogin', { userName, password }, isValidLogin);
+};
